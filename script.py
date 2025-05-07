a1b754b2887672a9cdb2f7baf420a321a39c29d7
Avigail <avigail.adam@intel.com>
2025-05-07 15:11:53 +0300

    minor

--- script.py
+++ script.py
@@ -1,2 +1,8 @@
-for i in range(5):
-    print(f'Number {i}')
+import json
+
+# Define the job names
+job_names = ["a", "b", "c"]
+
+# Output the job names as a JSON array
+with open("script_output.json", "w") as f:
+    json.dump(job_names, f)
