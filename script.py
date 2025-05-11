e530699dce1b6cc67b351f83f0af73ac571dccdf
Avigail <avigail.adam@intel.com>
2025-05-11 10:56:21 +0300

    minor

--- script.py
+++ script.py
@@ -1,12 +1,5 @@
-import json
-import os
-
-# Define the job names
 job_names = ["a", "b", "c"]
 
-# Get the workspace directory from the environment variable
-output_path = os.path.join(os.getenv("GITHUB_WORKSPACE", "."), "script_output.json")
+for j in job_names:
+    print(j)
 
-# Output the job names as a JSON array
-with open(output_path, "w") as f:
-    json.dump(job_names, f)
